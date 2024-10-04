466eda36acc75f5cf64b8a34d663e4f87a597eb9
Farhad Khurami <fkhurami98@gmail.com>
2024-10-04 13:00:44 +0000

    remove code

--- reddit_spider/spiders/reddit.py
+++ reddit_spider/spiders/reddit.py
@@ -2,8 +2,6 @@ import scrapy
 from reddit_spider.items import RedditPostItem
 from scrapy_playwright.page import PageMethod
 
-# def get_random_user_agent():
-#     return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
 class RedditSpider(scrapy.Spider):
     name = "reddit_spider"
@@ -17,12 +15,12 @@ class RedditSpider(scrapy.Spider):
                 meta={
                     "playwright": True,
                     "playwright_page_methods": [
-                        PageMethod("evaluate", "window.scrollBy(0, window.innerHeight * 60);"),
-                        PageMethod("wait_for_load_state", "networkidle"),  # Wait for network idle
-                        PageMethod("evaluate", "window.scrollBy(0, window.innerHeight * 60);"),  # Scroll down again
-                        PageMethod("wait_for_load_state", "networkidle"),  # Wait for network idle
+                        PageMethod(
+                            "evaluate", "window.scrollBy(0, window.innerHeight * 60);"
+                        ),
+                        PageMethod("wait_for_load_state", "networkidle"),
                     ],
-                }
+                },
             )
 
     async def parse(self, response):
@@ -30,17 +28,23 @@ class RedditSpider(scrapy.Spider):
 
         for post in posts:
             postItem = RedditPostItem()
-            postItem["title"] = post.attrib.get("post-title"),
-            postItem["author"] = post.attrib.get("author"),
-            postItem["comments"] = post.attrib.get("comment-count"),
-            postItem["permalink"] = post.attrib.get("permalink"),
+            postItem["title"] = post.attrib.get("post-title")
+            postItem["author"] = post.attrib.get("author")
+            postItem["comments"] = post.attrib.get("comment-count")
+            postItem["permalink"] = post.attrib.get("permalink")
             postItem["created_timestamp"] = post.attrib.get("created-timestamp")
             yield postItem
 
-        await response.follow(response.url, callback=self.parse, meta={
-            "playwright": True,
-            "playwright_page_methods": [
-                PageMethod("evaluate", "(window.scrollBy(0, document.body.scrollHeight))"),
-                PageMethod("wait_for_selector", "shreddit-post", timeout=10000),
-            ]
-        })
+        yield response.follow(
+            response.url,
+            callback=self.parse,
+            meta={
+                "playwright": True,
+                "playwright_page_methods": [
+                    PageMethod(
+                        "evaluate", "window.scrollBy(0, window.innerHeight * 60);"
+                    ),
+                    PageMethod("wait_for_selector", "shreddit-post", timeout=10000),
+                ],
+            },
+        )
