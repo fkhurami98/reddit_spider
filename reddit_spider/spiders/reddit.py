4af6037f27998d339e27b5e17f7c36f734b3f97a
Farhad Khurami <fkhurami98@gmail.com>
2024-10-06 17:42:39 +0000

    add functionality to log exit node ip

--- reddit_spider/spiders/reddit.py
+++ reddit_spider/spiders/reddit.py
@@ -8,7 +8,7 @@ import logging
 class RedditSpider(scrapy.Spider):
     name = "reddit_spider"
     start_urls = START_URLS
-    
+
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(
@@ -35,16 +35,29 @@ class RedditSpider(scrapy.Spider):
             )
 
     async def parse(self, response):
-        user_agent = response.request.headers.get("User-Agent").decode("utf-8")
-        logging.info(f"User-Agent: {user_agent}")
+        yield scrapy.Request(
+            url="https://httpbin.org/ip",
+            callback=self.log_exit_ip_and_continue,
+            meta={"original_response": response},
+            dont_filter=True,
+        )
 
-        proxy = response.meta.get("proxy")
+    def log_exit_ip_and_continue(self, response):
+        actual_exit_ip = response.json().get("origin")
+        original_response = response.meta["original_response"]
+        start_url = original_response.meta["start_url"]
+        proxy = original_response.meta.get("proxy")
+
+        user_agent = original_response.request.headers.get("User-Agent", b"").decode(
+            "utf-8"
+        )
+
+        print("\n" + "=" * 138)
+        logging.info(f"User-Agent used: {user_agent}")
+        logging.info(f"Exit IP (Tor Node): {actual_exit_ip}")
         if proxy:
-            logging.info(f"Exit IP (Proxy): {proxy}")
-
-
-        posts = response.css("shreddit-post")
-
+            logging.info(f"Proxy IP (Docker): {proxy}")
+        posts = original_response.css("shreddit-post")
         for post in posts:
             postItem = RedditPostItem()
             postItem["title"] = post.attrib.get("post-title")
@@ -52,10 +65,8 @@ class RedditSpider(scrapy.Spider):
             postItem["comments"] = post.attrib.get("comment-count")
             postItem["permalink"] = post.attrib.get("permalink")
             postItem["created_timestamp"] = post.attrib.get("created-timestamp")
-            postItem["start_url"] = response.meta["start_url"]
-
-            logging.info(
-                f"Scraped post: {postItem['title'][:40]} by {postItem['author']} from {postItem['start_url']}"
-            )
-
+            postItem["start_url"] = start_url
             yield postItem
+
+        logging.info(f"Scraped all posts from subreddit: {start_url} ")
+        print("=" * 138 + "\n")
